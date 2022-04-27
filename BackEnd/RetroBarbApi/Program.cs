25fbe6ef727b231463885ddbada5c4d9cc30224f
Sid Hinson <octobersith@outlook.com>
2022-04-27 16:16:52 -0400

    updated

--- BackEnd/RetroBarbApi/Program.cs
+++ BackEnd/RetroBarbApi/Program.cs
@@ -53,11 +53,6 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-app.UseCors(x=>x
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader());
-    
 app.UseAuthorization();
 
 app.MapControllers();
